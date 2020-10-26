a8da733e7296f1f3b4a9a7b54ef29fe038b0bb6c
yuancheng <yuancheng1222@live.com>
2020-10-26 09:41:55 +0800

    添加登录页面

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./shared/shared.module').then(m => m.SharedModule) },
+  { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
